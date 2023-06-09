256b99d9e180a74d9ed8a96ba7316f1c6f6492f7
alanlopez0208 <alan666_alan@hotmail.com>
2023-06-09 01:17:30 -0600

    Ya mero acabo

--- js/carrito.js
+++ js/carrito.js
@@ -1,71 +1,96 @@
-const contenedor = document.getElementById("shop-contenedor");
-const ids = JSON.parse(localStorage.getItem("id"));
+window.addEventListener("DOMContentLoaded", (event) => {
+  obtenerCarrito();
+  obtenerTotal();
+});
 
-if (ids != null) {
-  const tabla = document.createElement("table");
-  tabla.classList.add("tabla");
+async function obtenerCarrito() {
+  const contenedor = document.getElementById("shop-contenedor");
+  const ids = JSON.parse(localStorage.getItem("id"));
+  if (Object.keys(ids) != 0) {
+    const tabla = document.createElement("table");
+    tabla.classList.add("tabla");
 
-  tabla.innerHTML = `
-    <thead>
-      <tr>
-        <th>Producto</th>
-        <th>Cantidad</th>
-        <th>Precio Unitario</th>
-        <th>Precio Total</th>
-      </tr>
-    </thead>`;
+    tabla.innerHTML = `
+      <thead>
+        <tr>
+          <th>Producto</th>
+          <th>Cantidad</th>
+          <th>Precio Unitario</th>
+          <th>Precio Total</th>
+        </tr>
+      </thead>`;
 
-  const tbody = document.createElement("tbody");
+    const tbody = document.createElement("tbody");
 
-  for (let clave in ids) {
-    if (ids.hasOwnProperty(clave)) {
-      const filaProducto = crearFilaProducto();
-      tbody.appendChild(filaProducto);
+    for (let clave in ids) {
+      const API_URL = "http://localhost:5138";
+
+      for (const clave in ids) {
+        if (Object.hasOwnProperty.call(ids, clave)) {
+          try {
+            const response = await fetch(`${API_URL}/api/products/${clave}`, {
+              headers: {
+                Accept: "application/json",
+              },
+            });
+
+            if (!response.ok) {
+              throw new Error("Error en la solicitud");
+            }
+
+            const data = await response.json();
+            const producto = crearFilaProducto(data, ids[clave]);
+            tbody.appendChild(producto);
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      }
     }
-  }
 
-  tabla.appendChild(tbody);
-  contenedor.appendChild(tabla);
-} else {
-  const nuevoElemento = document.createElement("div");
-  nuevoElemento.classList.add("texto");
-  nuevoElemento.classList.add("advertencia");
-  nuevoElemento.innerHTML = `<p>No has seleccionado ningún artículo</p>`;
-  contenedor.appendChild(nuevoElemento);
+    tabla.appendChild(tbody);
+    contenedor.appendChild(tabla);
+  } else {
+    const nuevoElemento = document.createElement("div");
+    nuevoElemento.classList.add("texto");
+    nuevoElemento.classList.add("advertencia");
+    nuevoElemento.innerHTML = `<p>No has seleccionado ningún artículo</p>`;
+    contenedor.appendChild(nuevoElemento);
+  }
 }
 
-function crearFilaProducto() {
+function crearFilaProducto(data, cantidad) {
   const filaProducto = document.createElement("tr");
 
   const columnaProducto = document.createElement("td");
   columnaProducto.innerHTML = `
     <div class="producto">
-      <img src="img/productos/cubrebocas.png" alt="" />
-      <a href="#">Cubrebocas Perron de 5 tabletas</a>
+      <img src="${data.imgPath}" alt="" />
+      <a href="#">${data.name}</a>
     </div>`;
 
   const columnaCantidad = document.createElement("td");
   columnaCantidad.innerHTML = `
     <div class="cantidad">
-      <button onclick = "incrementar(this)" >+</button>
-      <input type="text" name="" id="cantidad-producto" readonly value = "1"/>
-      <button onclick = "decrementar(this)">-</button>
+      <button onclick = "incrementar(this,${data.id})" >+</button>
+      <input type="text" name="" id="cantidad-producto" readonly value = "${cantidad}"/>
+      <button onclick = "decrementar(this,${data.id})">-</button>
     </div>
   `;
 
   const columnaPrecioUnitario = document.createElement("td");
   columnaPrecioUnitario.classList.add("precioUnitario");
   columnaPrecioUnitario.dataset.titulo = "Precio Unitario";
-  columnaPrecioUnitario.innerText = "5.99";
+  columnaPrecioUnitario.innerText = data.price;
 
   const columnaPrecioTotal = document.createElement("td");
   columnaPrecioTotal.classList.add("PrecioTotal");
   columnaPrecioTotal.dataset.titulo = "Precio Total";
-  columnaPrecioTotal.innerText = "5.99";
+  columnaPrecioTotal.innerText = data.price * cantidad;
 
   const columnaBorrar = document.createElement("td");
 
-  columnaBorrar.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+  columnaBorrar.innerHTML = `<i class="fa-solid fa-trash"  onclick = "eliminarItem(this,${data.id})"></i>`;
 
   filaProducto.appendChild(columnaProducto);
 
@@ -76,7 +101,7 @@ function crearFilaProducto() {
   return filaProducto;
 }
 
-function incrementar(button) {
+function incrementar(button, idProducto) {
   const fila = button.closest("tr");
   const cantidadInput = fila.querySelector(".cantidad input");
   const precioUnitario = parseFloat(
@@ -92,9 +117,14 @@ function incrementar(button) {
 
   const nuevoPrecioTotal = precioUnitario * cantidad;
   fila.querySelector(".PrecioTotal").textContent = nuevoPrecioTotal.toFixed(2);
+  var local = localStorage.getItem("id");
+
+  var ids = JSON.parse(local);
+  ids[idProducto] = cantidad;
+  localStorage.setItem("id", JSON.stringify(ids));
 }
 
-function decrementar(button) {
+function decrementar(button, idProducto) {
   const fila = button.closest("tr");
   const cantidadInput = fila.querySelector(".cantidad input");
   const precioUnitario = parseFloat(
@@ -111,5 +141,53 @@ function decrementar(button) {
     const nuevoPrecioTotal = precioUnitario * cantidad;
     fila.querySelector(".PrecioTotal").textContent =
       nuevoPrecioTotal.toFixed(2);
+    var local = localStorage.getItem("id");
+    var ids = JSON.parse(local);
+    ids[idProducto] = cantidad;
+    localStorage.setItem("id", JSON.stringify(ids));
+  }
+}
+
+function eliminarItem(elemento, productoId) {
+  const fila = elemento.parentElement;
+  console.log(fila);
+  var local = localStorage.getItem("id");
+  var ids = JSON.parse(local);
+  delete ids[productoId];
+  console.log(ids);
+  localStorage.setItem("id", JSON.stringify(ids));
+  setTimeout(function () {
+    location.reload();
+  });
+}
+
+async function obtenerTotal() {
+  try {
+    let total = 0;
+    const localstorage = localStorage.getItem("id");
+    const id = JSON.parse(localstorage);
+
+    const API_URL = "http://localhost:5138";
+
+    const fetchPromises = [];
+
+    for (const clave in id) {
+      if (Object.hasOwnProperty.call(id, clave)) {
+        const response = await fetch(`${API_URL}/api/products/${clave}`, {
+          headers: {
+            Accept: "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Error en la solicitud");
+        }
+
+        const data = await response.json();
+        total += data.price * id[clave];
+      }
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
